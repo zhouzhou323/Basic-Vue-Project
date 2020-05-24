89ba155c3d2727f0d017104d306baedea9f38703
zhouzhou323 <zhouwq9596@gmail.com>
2020-05-25 00:12:56 +0800

    after weekend

--- 01-webpack/build/base.config.js
+++ 01-webpack/build/base.config.js
@@ -12,6 +12,9 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
+        // css-loader只负责将css文件进行加载
+        // style-loader负责将样式添加到dom
+        // 使用多个loader时是从右到左
                 use: ['style-loader', 'css-loader'],
               },
               {
@@ -33,15 +36,18 @@ module.exports = {
                     },
                   ],
               },
-            //   {
-            //     test: /\.(png|jpg|gif|jpeg)$/i,
-            //     use: [
-            //       {
-            //         loader: 'url-loader',
-            //         options: {
-            //           limit: 8192,
-            //         },
-            //       }]},
+              {
+                test: /\.(png|jpg|gif|jpeg)$/i,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+              // 当加载的图片小于limit时，会将图片编译成base64字符串形式
+              // 当加载的图片大于limit时，需要使用file-loader模块进行加载
+                      limit: 8192,
+                      name: 'img/[name].[hash:8].[ext]'
+                    },
+                  }]},
 
           {
             test: /\.m?js$/,
@@ -62,7 +68,8 @@ module.exports = {
     resolve:{
         alias:{
             'vue$':'vue/dist/vue.esm.js'
-        }
+        },
+        extensions:['.js','.css','.vue']
       },
     plugins:[
         new webpack.BannerPlugin('最终版权归粥粥所有'),
